5b01d46bcc171f3f9c811b6f105520530e1ed368
kadekaro <kadekaroshop@gmail.com>
2023-07-31 17:53:15 -0300

    3º commit - Primeiro desafio do curso de Python da Dio!

---
+++ Desafio de Código em Python.py
@@ -0,0 +1,39 @@
+"""
+Desafio 2/3:
+
+month = input()
+months_dict = {
+    '1': 'January', '2': 'February', '3': 'March', '4': 'April', '5': 'May', '6': 'June',
+    '7': 'July', '8': 'August', '9': 'September', '10': 'October', '11': 'November', '12': 'December'
+}
+
+for i, j in enumerate(months_dict):
+    if j == month:
+        print(months_dict[j])
+
+
+if month in months_dict.keys():
+    print(months_dict[month])
+"""
+
+"""Desafio 3/3:"""
+
+
+def encaixa(A, B):
+    if len(A) < len(B):
+        return "nao encaixa"
+
+    if A[-len(B):] == B:
+        return "encaixa"
+    else:
+        return "nao encaixa"
+
+
+N = int(input())
+
+i = 0
+while i < N:
+    A, B = input().split()
+    resultado = encaixa(A, B)
+    print(resultado)
+    i += 1
